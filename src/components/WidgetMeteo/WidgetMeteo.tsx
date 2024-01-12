a111931db7a4e448c1fbc42060f29e7f4ba05d4e
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-01-12 18:29:21 +0100

    Déplacement de la déclaration de fontion dans le useEffect et ajout de zipcode en dpendance

--- src/components/WidgetMeteo/WidgetMeteo.tsx
+++ src/components/WidgetMeteo/WidgetMeteo.tsx
@@ -10,19 +10,18 @@ interface WidgetMeteoProps {
 function WidgetMeteo( { city, zipcode }: WidgetMeteoProps) {
 
     const [temperature, setTemperature] = useState(0)
-
-    const fetchTemperature = async () => {
-        const API_KEY = '47840f4f526d9cc69b4b575c52495860'
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},fr&appid=${API_KEY}&units=metric`)
-        console.log(result)
-        setTemperature(Math.round(result.data.main.temp*10)/10)
-    }
-
+    
     useEffect(
         () => {
+            const fetchTemperature = async () => {
+                const API_KEY = '47840f4f526d9cc69b4b575c52495860'
+                const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},fr&appid=${API_KEY}&units=metric`)
+                console.log(result)
+                setTemperature(Math.round(result.data.main.temp*10)/10)
+            }
             fetchTemperature() // appel API
         },
-        [] // uniquement au premier rendu
+        [zipcode] // uniquement au premier rendu
     )
 
     return (
@@ -30,7 +29,7 @@ function WidgetMeteo( { city, zipcode }: WidgetMeteoProps) {
             <div>
                 <div className="meteo-city">{city}</div>
                 <div className="meteo-zipcode">{zipcode}</div>
-                <div className="meteo-temperature">{temperature}</div>
+                <div className="meteo-temperature">{temperature}°C</div>
             </div>
         </div>
     )
