4188ff306aff6f97ec02d2076f91d6529d281384
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-01-12 18:13:28 +0100

    Appel ajax dans un useEffect et actualisation temperature dans le state

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,11 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <h2>Bienvenue sur le météo widget de Sylvia</h2>
-        <WidgetMeteo city="Saint-Etienne" />
-        <WidgetMeteo city="Ivry-sur-Seine" />
-      </div>
-    </>
+    <div>
+      <h2>Bienvenue sur le widget météo de Sylvia</h2>
+      <WidgetMeteo city="Saint-Etienne" zipcode="42000" />
+      <WidgetMeteo city="Ivry-sur-Seine" zipcode="94200" />
+    </div>
   )
 }
 
--- src/components/WidgetMeteo/WidgetMeteo.tsx
+++ src/components/WidgetMeteo/WidgetMeteo.tsx
@@ -1,9 +1,37 @@
-function WidgetMeteo() {
+import { useEffect, useState } from 'react'
+import './WidgetMeteo.scss'
+import axios from 'axios'
+
+interface WidgetMeteoProps {
+    city: string
+    zipcode: string
+}
+
+function WidgetMeteo( { city, zipcode }: WidgetMeteoProps) {
+
+    const [temperature, setTemperature] = useState(0)
+
+    const fetchTemperature = async () => {
+        const API_KEY = '47840f4f526d9cc69b4b575c52495860'
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},fr&appid=${API_KEY}&units=metric`)
+        console.log(result)
+        setTemperature(Math.round(result.data.main.temp*10)/10)
+    }
+
+    useEffect(
+        () => {
+            fetchTemperature() // appel API
+        },
+        [] // uniquement au premier rendu
+    )
+
     return (
         <div className="meteo">
-            <div className="meteo-city">Saint-Etienne</div>
-            <div className="meteo-zipcode">42000</div>
-            <div className="meteo-temperature">-1°C</div>
+            <div>
+                <div className="meteo-city">{city}</div>
+                <div className="meteo-zipcode">{zipcode}</div>
+                <div className="meteo-temperature">{temperature}</div>
+            </div>
         </div>
     )
 }
